9e82cfb0c044e6e8d21b837ea71ce966ac46a189
Volodymyr025 <Fudo2790@gmail.com>
2024-10-07 10:47:35 +0300

    wip

--- config/database.ts
+++ config/database.ts
@@ -3,12 +3,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
+      host: env("DATABASE_HOST"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "bank"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "0000"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      schema: env("DATABASE_SCHEMA"), // Not required
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
       },
